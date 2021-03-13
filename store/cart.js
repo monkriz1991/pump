faa23b97ad01ec6b78217409edfb6b9dd6897f8f
chitsalex <chitsalex@gmail.com>
2021-03-13 16:48:41 +0300

    store для корзины

---
+++ store/cart.js
@@ -0,0 +1,77 @@
+/**
+ * 
+ * Функция получает из cooke список товаров добавленых в карзину
+ * 
+ * @returns {object} 
+ */
+function getCartFromLocalStorage(){
+    if(process.browser){
+       let carts =  JSON.parse(localStorage.getItem("carts"));
+       if(carts==null){
+        return [];
+       }else{
+        return carts;
+       }
+      
+    }
+    return [];
+ }
+
+ /**
+  * 
+  * Функция помещает в cookie список товаров карзины пользователя 
+  * 
+  * @param {object} carts массив товров для корзины
+  */
+ function setCartToLocalStorage(carts){
+    if(process.browser){
+        localStorage.setItem("carts", JSON.stringify(carts));
+    }
+ }
+
+export default {
+    namespaced: true,
+  
+    state: () => ({
+    carts: [],
+    counter:0,
+  }),
+  getters: {
+
+  },
+ mutations : {
+     /**
+      * 
+      * метод добавяет в карзину товар
+      * 
+      * @param {*} state хранилище
+      * @param {*} cart объект товара
+      */
+    addCart(state, cart){
+        let carts=getCartFromLocalStorage();
+        carts.push(cart);
+        state.carts = carts;
+        state.counter = state.cart.length;
+        setCartToLocalStorage(state.carts);
+    },
+    /**
+     * функция удаления товара из корзины
+     * @param {*} state хранилище
+     * @param {*} id идкнтификатор товара
+     */
+    delCart(state, id){
+        state.carts=getCartFromLocalStorage();
+        state.carts=state.cart.filter(x=>x.id!=id);
+        state.counter = state.cart.length;
+        setCartToLocalStorage(state.carts);
+    }
+  },
+
+  actions: {
+        getCart(){
+            let carts=getCartFromLocalStorage();
+            return carts;
+        }
+  }
+}
+
